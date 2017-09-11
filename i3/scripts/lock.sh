1d9b76288bb8a69d90b560d37171f503c18b389e
Project 9501 <project9501@gmail.com>
2017-09-11 19:56:27 +0000

    changes to i3 config

--- i3/scripts/lock.sh
+++ i3/scripts/lock.sh
@@ -3,7 +3,9 @@
 ## as a lock screen with i3lock
 
 # Take screencap
-scrot /tmp/screen.png
+[ -f /tmp/screen.png ] && rm /tmp/screen.png
+
+scrot /tmp/screen.png && chmod 666 /tmp/screen.png
 
 convert /tmp/screen.png -paint 3 /tmp/screen.png
 [[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png  ~/.config/i3/lock.png -gravity center -composite -matte /tmp/screen.png
